08243e43df3e27066b139c6c4c487fbea0e6c6f5
hattohattohatto <h.kobayashi@hajimari.inc>
2022-07-20 12:11:42 +0900

    vアリデーション範囲の修正

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -14,7 +14,7 @@ class CommentsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('comment.validate')->only(['store', 'update']);
+        $this->middleware('comment.validate')->only(['store']);
     }
 
     /**
