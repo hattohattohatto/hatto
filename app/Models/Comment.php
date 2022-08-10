6ff218c13c7be41c3f4c8bed0ff1785141597c9d
hattohattohatto <h.kobayashi@hajimari.inc>
2022-08-10 15:54:51 +0900

    画像表示の対策

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,12 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\softDeletes;
 
 class Comment extends Model
 {
-    use SoftDeletes;
-
     /**
      * ホワイトリストのセット
      *
